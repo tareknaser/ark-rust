a163a3ab2db79e3e288f9bd88d94ef3daee31b65
Tarek <tareknaser360@gmail.com>
2024-09-09 11:45:14 +0300

    feat: ResourceIndex watch API
    Signed-off-by: Tarek <tareknaser360@gmail.com>

--- fs-index/src/lib.rs
+++ fs-index/src/lib.rs
@@ -1,10 +1,11 @@
 mod index;
 mod serde;
 mod utils;
-
-pub use utils::load_or_build_index;
+mod watch;
 
 pub use index::ResourceIndex;
+pub use utils::load_or_build_index;
+pub use watch::watch_index;
 
 #[cfg(test)]
 mod tests;
---
+++ fs-index/src/watch.rs
@@ -0,0 +1,107 @@
+use std::{fs, path::Path};
+
+use anyhow::Result;
+use futures::{
+    channel::mpsc::{channel, Receiver},
+    SinkExt, StreamExt,
+};
+use log::info;
+use notify::{Config, Event, RecommendedWatcher, RecursiveMode, Watcher};
+
+use data_resource::ResourceId;
+use fs_storage::ARK_FOLDER;
+
+use crate::ResourceIndex;
+
+/// Watches a given directory for file system changes and automatically updates
+/// the resource index.
+///
+/// This function continuously monitors the specified directory and responds to
+/// file system events such as file creation, modification, and deletion. When
+/// an event is detected, the function updates the associated resource index and
+/// stores the changes.
+///
+/// The function runs asynchronously, whcih makes it suitable for non-blocking
+/// contexts. It uses a recursive watcher to track all changes within the
+/// directory tree. Events related to the internal `.ark` folder are ignored to
+/// prevent unnecessary updates.
+///
+/// # Arguments
+///
+/// * `root_path` - The root directory to be watched. This path is canonicalized
+///   to handle symbolic links and relative paths correctly.
+pub async fn watch_index<P: AsRef<Path>, Id: ResourceId>(
+    root_path: P,
+) -> Result<()> {
+    log::debug!(
+        "Attempting to watch index at root path: {:?}",
+        root_path.as_ref()
+    );
+
+    let root_path = fs::canonicalize(root_path.as_ref())?;
+    let mut index: ResourceIndex<Id> = ResourceIndex::build(&root_path)?;
+    index.store()?;
+
+    let (mut watcher, mut rx) = async_watcher()?;
+    info!("Watching directory: {:?}", root_path);
+    let config = Config::default();
+    watcher.configure(config)?;
+    watcher.watch(root_path.as_ref(), RecursiveMode::Recursive)?;
+    info!("Started watcher with config: \n\t{:?}", config);
+
+    let ark_folder = root_path.join(ARK_FOLDER);
+    while let Some(res) = rx.next().await {
+        match res {
+            Ok(event) => {
+                // If the event is a change in .ark folder, ignore it
+                if event
+                    .paths
+                    .iter()
+                    .any(|p| p.starts_with(&ark_folder))
+                {
+                    continue;
+                }
+                // If the event is not a create, modify or remove, ignore it
+                if !(event.kind.is_create()
+                    || event.kind.is_modify()
+                    || event.kind.is_remove())
+                {
+                    continue;
+                }
+
+                info!("Detected event: {:?}", event);
+                let file = event
+                    .paths
+                    .first()
+                    .expect("Failed to get file path from event");
+                log::debug!("Updating index for file: {:?}", file);
+                let relative_path = file.strip_prefix(&root_path)?;
+                index.update_one(&relative_path)?;
+
+                index.store()?;
+                info!("Index updated and stored");
+            }
+            Err(e) => log::error!("Error in watcher: {:?}", e),
+        }
+    }
+
+    unreachable!("Watcher stream ended unexpectedly");
+}
+
+fn async_watcher(
+) -> notify::Result<(RecommendedWatcher, Receiver<notify::Result<Event>>)> {
+    let (mut tx, rx) = channel(1);
+
+    let watcher = RecommendedWatcher::new(
+        move |res| {
+            futures::executor::block_on(async {
+                if let Err(err) = tx.send(res).await {
+                    log::error!("Error sending event: {:?}", err);
+                }
+            })
+        },
+        Config::default(),
+    )?;
+
+    Ok((watcher, rx))
+}
